07065e4129b3d62b46a3f3f79fb76dda71efcdd5
sagar <bansalsagar13@gmail.com>
2022-09-28 22:39:08 +0530

    database connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 //connected to database
 mongoose.connect('mongodb+srv://skbansal:skbnsl@cluster0.i2grcwf.mongodb.net/?retryWrites=true&w=majority');
-
+                
 const db = mongoose.connection;
 
 //checking that db is connected or not
